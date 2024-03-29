7933650c6c83d349eb3492c5bf6739048934dd9a
Kappaccinoh <lim.jia28@gmail.com>
2024-03-29 16:24:12 +0800

    add junit test

---
+++ src/test/java/seedu/address/MainAppTest.java
@@ -0,0 +1,28 @@
+package seedu.address;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Contains the test cases for the file MainApp
+ */
+public class MainAppTest {
+    @Test
+    public void mainapp_runssuccessfully() {
+        MainApp main = new MainApp();
+    }
+
+    @Test
+    public void mainapp_returnscorrectversion() {
+        MainApp main = new MainApp();
+        assertEquals(main.VERSION.toString(), "V1.2.1ea");
+    }
+
+    @Test
+    public void mainapp_loggerexists() {
+        MainApp main = new MainApp();
+    }
+}
