3063c8fb2dad077b7540b1335b3e3d499a7aeae7
Alteqa <e0969816@u.nus.edu>
2024-04-03 12:51:22 +0800

    Resolve comments

--- src/main/java/seedu/address/model/appointment/Appointment.java
+++ src/main/java/seedu/address/model/appointment/Appointment.java
@@ -9,6 +9,8 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
 
+import javax.script.ScriptEngine;
+
 /**
  * Appointment class that describes an appointment
  */
--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -1,68 +1,63 @@
-//package seedu.address.logic.commands;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
-//import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class EditPersonDescriptorTest {
-//
-//    @Test
-//    public void equals() {
-//        // same values -> returns true
-//        EditPersonDescriptor descriptorWithSameValues = new EditPersonDescriptor(DESC_AMY);
-//        assertTrue(DESC_AMY.equals(descriptorWithSameValues));
-//
-//        // same object -> returns true
-//        assertTrue(DESC_AMY.equals(DESC_AMY));
-//
-//        // null -> returns false
-//        assertFalse(DESC_AMY.equals(null));
-//
-//        // different types -> returns false
-//        assertFalse(DESC_AMY.equals(5));
-//
-//        // different values -> returns false
-//        assertFalse(DESC_AMY.equals(DESC_BOB));
-//
-//        // different name -> returns false
-//        EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB).build();
-//        assertFalse(DESC_AMY.equals(editedAmy));
-//
-//        // different phone -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
-//        assertFalse(DESC_AMY.equals(editedAmy));
-//
-//        // different email -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withEmail(VALID_EMAIL_BOB).build();
-//        assertFalse(DESC_AMY.equals(editedAmy));
-//
-//        // different address -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withAddress(VALID_ADDRESS_BOB).build();
-//        assertFalse(DESC_AMY.equals(editedAmy));
-//
-//        // different tags -> returns false
-//        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
-//        assertFalse(DESC_AMY.equals(editedAmy));
-//    }
-//
-//    @Test
-//    public void toStringMethod() {
-//        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
-//        String expected = EditPersonDescriptor.class.getCanonicalName() + "{name="
-//                + editPersonDescriptor.getName().orElse(null) + ", phone="
-//                + editPersonDescriptor.getPhone().orElse(null) + ", email="
-//                + editPersonDescriptor.getEmail().orElse(null) + ", address="
-//                + editPersonDescriptor.getAddress().orElse(null) + ", tags="
-//                + editPersonDescriptor.getTags().orElse(null) + "}";
-//        assertEquals(expected, editPersonDescriptor.toString());
-//    }
-//}
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DOB_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.testutil.EditPersonDescriptorBuilder;
+
+public class EditPersonDescriptorTest {
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        EditCommand.EditPersonDescriptor descriptorWithSameValues = new EditCommand.EditPersonDescriptor(DESC_AMY);
+        assertTrue(DESC_AMY.equals(descriptorWithSameValues));
+
+        // same object -> returns true
+        assertTrue(DESC_AMY.equals(DESC_AMY));
+
+        // null -> returns false
+        assertFalse(DESC_AMY.equals(null));
+
+        // different types -> returns false
+        assertFalse(DESC_AMY.equals(5));
+
+        // different values -> returns false
+        assertFalse(DESC_AMY.equals(DESC_BOB));
+
+        // different name -> returns false
+        EditCommand.EditPersonDescriptor editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withName(VALID_NAME_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different phone -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withPhone(VALID_PHONE_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different nric -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withNric(VALID_NRIC_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+
+        // different dob -> returns false
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withDob(VALID_DOB_BOB).build();
+        assertFalse(DESC_AMY.equals(editedAmy));
+    }
+
+    @Test
+    public void toStringMethod() {
+        EditCommand.EditPersonDescriptor editPersonDescriptor = new EditCommand.EditPersonDescriptor();
+        String expected = EditCommand.EditPersonDescriptor.class.getCanonicalName() + "{name="
+                + editPersonDescriptor.getName().orElse(null) + ", phone="
+                + editPersonDescriptor.getPhone().orElse(null) + ", nric="
+                + editPersonDescriptor.getNric().orElse(null) + ", dob="
+                + editPersonDescriptor.getDob().orElse(null)  + "}";
+        assertEquals(expected, editPersonDescriptor.toString());
+    }
+}
