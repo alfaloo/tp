e36b1eedc41110306b7607335f213abbecd9003a
Cheng-Yu Dong <officialchengyud@gmail.com>
2024-04-04 19:53:35 +0800

    Update doctor command usage

--- src/main/java/seedu/address/logic/commands/QueryDoctorCommand.java
+++ src/main/java/seedu/address/logic/commands/QueryDoctorCommand.java
@@ -16,7 +16,7 @@ public class QueryDoctorCommand extends Command {
 
     public static final String COMMAND_WORD = "doctor";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all doctors whose names contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all doctors whose names or NRIC contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
